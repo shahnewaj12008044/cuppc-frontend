5f74c3238a1aa1cba2b5ae4323d6ee99e68be748
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-08-27 14:06:59 +0600

    join club is done

--- src/components/JoinClub.jsx
+++ src/components/JoinClub.jsx
@@ -1,11 +1,14 @@
-
+import FormfacadeEmbed from "@formfacade/embed-react";
 
 const JoinClub = () => {
-    return (
-        <div>
-            welcome to join club
-        </div>
-    );
+  return (
+    <div className="my-container h-screen">
+      <FormfacadeEmbed
+        formFacadeURL="https://formfacade.com/include/103709156760937366400/form/1FAIpQLSdLVskPbPyLOnIu5sQXNUKLQucvkgAwDBwzG_hfMARjll9Ojg/classic.js/?div=ff-compose"
+        onSubmitForm={() => console.log("Form submitted")}
+      />
+    </div>
+  );
 };
 
 export default JoinClub;
