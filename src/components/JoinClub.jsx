a7dbbf8a1c4645605e816065c9a50bde73bf7c82
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-09-16 03:08:15 +0600

    frontend modified lil bit

--- src/components/JoinClub.jsx
+++ src/components/JoinClub.jsx
@@ -2,7 +2,7 @@ import FormfacadeEmbed from "@formfacade/embed-react";
 
 const JoinClub = () => {
   return (
-    <div className="my-container h-screen">
+    <div className="my-container h-full">
       <FormfacadeEmbed
         formFacadeURL="https://formfacade.com/include/103709156760937366400/form/1FAIpQLSdLVskPbPyLOnIu5sQXNUKLQucvkgAwDBwzG_hfMARjll9Ojg/classic.js/?div=ff-compose"
         onSubmitForm={() => console.log("Form submitted")}
